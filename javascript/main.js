c3f2196b9ec0c78e4a76b1b77d5b0605e8af1318
Nikolay Panev <panevnikolay@gmail.com>
2020-12-14 15:42:20 +0100

    js browser build test

---
+++ javascript/main.js
@@ -0,0 +1,16 @@
+const a = 4
+const b = 5
+
+console.log(4+5)
+
+console.log("This was the result")
+
+x = 10
+
+if (x < 10) {
+    console.log("x is smaller than 10")
+} else if (x > 10) {
+    console.log("x is greater than 10") 
+} else {
+    console.log("x is 10")
+}
