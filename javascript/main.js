7522857b2f60f291bd9f16dc316296d3d8dacc8c
Nikolay Panev <panevnikolay@gmail.com>
2020-12-15 17:15:41 +0100

    added function examples

--- javascript/main.js
+++ javascript/main.js
@@ -1,26 +1,61 @@
-const a = 4
-const b = 5
+const companies = [
+        {name: "Company One", category: "Finance", start: 1981, end: 2004},
+        {name: "Company Two", category: "Retail", start: 1992, end: 2008},
+        {name: "Company Three", category: "Auto", start: 1999, end: 2007},
+        {name: "Company Four", category: "Retail", start: 1989, end: 2010},
+        {name: "Company Five", category: "Technology", start: 2009, end: 2014},
+        {name: "Company Six", category: "Finance", start: 1987, end: 2010},
+        {name: "Company Seven", category: "Auto", start: 1986, end: 1996},
+        {name: "Company Eight", category: "Technology", start: 2011, end: 2016},
+        {name: "Company Nine", category: "Retail", start: 1981, end: 1989}
+      ];
+    
+const ages = [33, 12, 20, 16, 5, 54, 21, 44, 61, 13, 15, 45, 25, 64, 32];
 
-console.log(4+5)
-
-console.log("This was the result")
-
-x = 10
-
-if (x < 10) {
-    console.log("x is smaller than 10")
-} else if (x > 10) {
-    console.log("x is greater than 10") 
-} else {
-    console.log("x is 10")
+// basic for loop
+for(let i = 0; i < companies.length; i++) {
+    console.log(companies[i]);
 }
 
-function Person(firstName, lastName, birthday) {
-    this.firstName = firstName;
-    this.lastName = lastName;
-    this.birthday = new Date(birthday);
+// forEach loop
+companies.forEach(function(company) {
+    console.log(company);
+});
+
+// filter - filter all ages less than 18 from the array
+// solution with a for loop
+let canDrink = []
+for(let i = 0; i < ages.length; i++) {
+    if(ages[i] >= 18) {
+        canDrink.push(ages[i])
+    }
 }
 
-const person1 = new Person('John', 'Doe', '3-4-1980');
+// direct filter: get 18 and older
+const canDrink = ages.filter(function(age) {
+    if(age >= 18) {
+        return true;
+    }
+})
 
-console.log(person1.birthday)
+// arrow function
+const canDrink = ages.filter(age => age >= 18);
+
+// filter retail companies
+
+const retailCompanies = companies.filter(function(company) {
+    if (company.category === 'Retail') {
+        return true;
+    }
+})
+
+const retailCompanies = companies.filter(company => category === 'retail')
+
+console.log(retailCompanies);
+
+
+
+console.log(canDrink)
+
+//
+
